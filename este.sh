0b70cb46a108210323035e52b4c165b8a1172c53
Marco Velez <61840865+McVelez@users.noreply.github.com>
2023-06-07 01:44:39 -0700

    Add files via upload

---
+++ este.sh
@@ -0,0 +1,16 @@
+wget https://raw.githubusercontent.com/McVelez/MonitoreoReport/832e59f9e677e0a6becd9d7888d64355bd532fd3/EmailFlow.PNG
+wget https://raw.githubusercontent.com/McVelez/MonitoreoReport/832e59f9e677e0a6becd9d7888d64355bd532fd3/EmailFlow2.PNG
+wget https://raw.githubusercontent.com/McVelez/MonitoreoReport/blob/main/Lan1_debian.html
+wget https://raw.githubusercontent.com/McVelez/MonitoreoReport/blob/main/Lan1_do.html
+wget https://raw.githubusercontent.com/McVelez/MonitoreoReport/blob/main/Lan1_pfsense.PNG
+wget https://raw.githubusercontent.com/McVelez/MonitoreoReport/blob/main/Lan1_zabbix.PNG
+wget https://raw.githubusercontent.com/McVelez/MonitoreoReport/blob/main/Lan2_debian.PNG
+wget https://raw.githubusercontent.com/McVelez/MonitoreoReport/blob/main/Lan2_do.PNG
+wget https://raw.githubusercontent.com/McVelez/MonitoreoReport/blob/main/Lan2_pfsense.PNG
+wget https://raw.githubusercontent.com/McVelez/MonitoreoReport/blob/main/Lan2_prtg.PNG
+wget https://raw.githubusercontent.com/McVelez/MonitoreoReport/832e59f9e677e0a6becd9d7888d64355bd532fd3/TelegramFlow.PNG
+wget https://raw.githubusercontent.com/McVelez/MonitoreoReport/832e59f9e677e0a6becd9d7888d64355bd532fd3/TelegramFlow2.PNG
+wget https://raw.githubusercontent.com/McVelez/MonitoreoReport/832e59f9e677e0a6becd9d7888d64355bd532fd3/index.html
+
+
+
